68d08016db963a4051b87deccdf6e9845f7d9a35
Paris Nakita Kejser <contact@parisnakitakejser.com>
2020-03-08 19:49:30 +0100

    add part 14 code

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 
 from library.flask.users import FlaskUsers
 from library.flask.system import FlaskSystem
@@ -7,6 +8,13 @@ from library.flask.system import FlaskSystem
 prefix_path = os.getenv('FAMILY_ORGANIZER_PREFIX_URL', '/api/')
 
 app = Flask(__name__)
+CORS(app)
+cros = CORS(app, resources={
+    r"/api/*": {
+        "origins": "*"
+    }
+})
+
 app.add_url_rule(f'{prefix_path}users', view_func=FlaskUsers.users, methods=['GET', 'POST', 'PUT'])
 app.add_url_rule(f'{prefix_path}system/status-verify', view_func=FlaskSystem.status_verify, methods=['GET'])
 app.add_url_rule(f'{prefix_path}system/configuration/completed', view_func=FlaskSystem.configuration_completed, methods=['PUT'])
